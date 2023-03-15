1005c937406481bf0cf4c426d42cab2d83780a9a
rusa9999 <menrusamen19992@gmail.com>
2023-03-16 00:02:48 +0500

    fix

--- config/database.js
+++ config/database.js
@@ -7,7 +7,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", true)
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
   },
 });
